82fefba1606577af01da072c97540eb4da5328f4
user name <user email>
2022-11-07 21:45:27 +0000

    updating the chat app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ChatPage from './components/ChatPage';
+import Home from './components/Home';
+
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://localhost:3001');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div  >
+        <Routes>
+        <Route path='/' element={<Home socket={socket} />}></Route>
+        <Route path='/chat' element={<ChatPage socket={socket} />}></Route>
+
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
